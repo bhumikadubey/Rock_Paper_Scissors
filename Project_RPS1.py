950b58a2e85c9386ce324642a21d4f8fd32dea9e
bhumikadubey <bhoomidubey13@gmail.com>
2023-04-02 17:19:15 +0530

    initial commit
    Signed-off-by: bhumikadubey <bhoomidubey13@gmail.com>

---
+++ Project_RPS1.py
@@ -0,0 +1,49 @@
+# Rock Paper Scissor
+import random
+def gameWin(comp, you):
+    # If two values are equal, declare a tie!
+    if comp == you:
+        return None
+
+    # Check for all posibilities when computer choose r
+    elif comp == 'r':
+        if you == 's':
+            return False
+        elif you == 'p':
+            return True
+
+    # Check for all posiblities when computer choose p
+    elif comp == 'p':
+        if you == 's':
+            return False
+        elif you == 'r':
+            return True
+    
+    # Check for all posiblities when computer choose s
+    elif comp == 's':
+        if you == 'p':
+            return False
+        elif you == 'r':
+            return True
+
+print("Comp Turn: Rock(r) Paper(p) or Scissor(s)?")
+randNo = random.randint(1, 3)
+if randNo == 1:
+    comp = 'r'
+elif randNo == 2:
+    comp = 'p'
+elif randNo == 3:
+    comp = 's'
+
+you = input("Your Turn: Rock(r) Paper(p) or Scissor(s)?")
+a = gameWin(comp, you)
+
+print(f"Computer choose {comp}")
+print(f"You choose {you}")
+
+if a == None:
+    print("The game is a tie!")
+elif a:
+    print("You Win!")
+else:
+    print("You Lose!")
